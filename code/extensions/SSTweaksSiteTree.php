17987c5222e9110f3401a815d8340c5b04c16127
Morven Lewis-Everley <morven@i-lateral.com>
2014-03-26 10:19:39 +0000

    Ensure that we remove any instances of Summary Info on sitetree before re-adding

--- code/extensions/SSTweaksSiteTree.php
+++ code/extensions/SSTweaksSiteTree.php
@@ -7,37 +7,40 @@
 class SSTweaksSiteTree extends DataExtension {
     public static $db = array(
         "SummaryContent"    => "Text",
-		"ShowChildren"      => "Boolean",
-		"ShowContact"       => "Boolean"
+        "ShowChildren"      => "Boolean",
+        "ShowContact"       => "Boolean"
     );
-    
+
     public static $has_one = array(
         "SummaryImage"      => "Image"
     );
 
     function updateCMSFields(FieldList $fields) {
+        $fields->removeByName("SummaryContent");
+        $fields->removeByName("SummaryImage");
+
         $summary_fields = ToggleCompositeField::create('Summary', 'Summary Info.',
-			array(
-				TextareaField::create("SummaryContent", $this->owner->fieldLabel('SummaryContent')),
-				UploadField::create("SummaryImage", $this->owner->fieldLabel('SummaryImage'))
-			)
-		)->setHeadingLevel(4);
-        
+            array(
+                TextareaField::create("SummaryContent", $this->owner->fieldLabel('SummaryContent')),
+                UploadField::create("SummaryImage", $this->owner->fieldLabel('SummaryImage'))
+            )
+        )->setHeadingLevel(4);
+
         $fields->addFieldToTab('Root.Main', $summary_fields, 'Metadata');
     }
 
     function updateSettingsFields(FieldList $fields) {
         $children = FieldGroup::create(
-	        CheckboxField::create('ShowChildren', 'Show children in the content area?')
-		)->setTitle('Children of this page');
-		
-	    $fields->addFieldToTab('Root.Settings', $children);
-	    
+            CheckboxField::create('ShowChildren', 'Show children in the content area?')
+        )->setTitle('Children of this page');
+
+        $fields->addFieldToTab('Root.Settings', $children);
+
         $contact = FieldGroup::create(
-	        CheckboxField::create('ShowContact', 'Show contact info on this page?')
-		)->setTitle('Contact Info (from settings)');
-		
-	    $fields->addFieldToTab('Root.Settings', $contact);
+            CheckboxField::create('ShowContact', 'Show contact info on this page?')
+        )->setTitle('Contact Info (from settings)');
+
+        $fields->addFieldToTab('Root.Settings', $contact);
     }
 }
 
