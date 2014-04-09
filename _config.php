a8c5f456cba60b9a0c62fd0971451644b746471a
Morven Lewis-Everley <morven@i-lateral.com>
2014-04-09 21:12:55 +0100

    Move social nav into controller, not content controller

--- _config.php
+++ _config.php
@@ -3,4 +3,3 @@
 SiteConfig::add_extension('SSTweaksSiteConfig');
 SiteTree::add_extension('SSTweaksSiteTree');
 Controller::add_extension('SSTweaksController');
-ContentController::add_extension('SSTweaksContentController');
--- code/extensions/SSTweaksController.php
+++ code/extensions/SSTweaksController.php
@@ -6,6 +6,23 @@
  */
 class SSTweaksController extends Extension {
 
+    public function SocialNav() {
+        $config = SiteConfig::current_site_config();
+
+        $vars = array(
+            "Facebook"  => ($config->FacebookURL) ? $config->FacebookURL : "",
+            "Twitter"   => ($config->TwitterURL) ? $config->TwitterURL : "",
+            "GooglePlus"=> ($config->GooglePlusURL) ? $config->GooglePlusURL : "",
+            "LinkdIn"   => ($config->LinkdInURL) ? $config->LinkdInURL : "",
+            "YouTube"   => ($config->YouTubeURL) ? $config->YouTubeURL : "",
+            "Pinterest" => ($config->PinterestURL) ? $config->PinterestURL : ""
+        );
+
+        $this->owner->extend("UpdateSocialNav", $vars);
+
+        return $this->owner->renderWith("SocialNav", $vars);
+    }
+
     /**
      * Set a flash message that will appear in your templates
      *
