e08599047aa11ee25b5df129fbcb55b314b4af19
Morven Lewis-Everley <morven@i-lateral.com>
2016-04-11 13:09:51 +0100

    Remove contact info and add depreciation warnings

--- code/extensions/SSTweaksSiteConfig.php
+++ code/extensions/SSTweaksSiteConfig.php
@@ -5,18 +5,42 @@
  * @author morven
  */
 class SSTweaksSiteConfig extends DataExtension {
-    public static $db = array(
+    
+    static $db = array(
         "FooterContent"         => "HTMLText",
-        "ContactEmail"          => "Varchar(100)",
-        "ContactPhone"          => "Varchar(50)",
-        "ContactAddress"        => "Text",
-        "MiscContactInfo"       => "Text",
         "MapHTML"               => "HTMLText",
     );
 
-    public static $has_one = array(
+    static $has_one = array(
         'Logo'          => 'Image'
     );
+    
+    static $casting = array(
+        "ContactEmail"          => "Varchar(100)",
+        "ContactPhone"          => "Varchar(50)",
+        "ContactAddress"        => "Text",
+        "MiscContactInfo"       => "Text"
+    );
+    
+    public function ContactEmail() {
+        Deprecation::notice('3.0', 'SSTweaks.ContactEmail is discontinued, please Add this yourself it you need it.');
+    }
+    
+    public function ContactPhone() {
+        Deprecation::notice('3.0', 'SSTweaks.ContactPhone is discontinued, please Add this yourself it you need it.');
+    }
+    
+    public function ContactAddress() {
+        Deprecation::notice('3.0', 'SSTweaks.ContactAddress is discontinued, please Add this yourself it you need it.');
+    }
+    
+    public function MiscContactInfo() {
+        Deprecation::notice('3.0', 'SSTweaks.MiscContactInfo is discontinued, please Add this yourself it you need it.');
+    }
+    
+    public function MapHTML() {
+        Deprecation::notice('3.0', 'SSTweaks.MapHTML is discontinued, please Add this yourself it you need it.');
+    }
 
     public function ContactAddressXML() {
         return (nl2br(Convert::raw2xml ($this->owner->ContactAddress), true));
@@ -30,19 +54,9 @@ class SSTweaksSiteConfig extends DataExtension {
         $fields->addFieldToTab('Root.Main', UploadField::create('Logo')->setFolderName('logos'));
 
 
-        $footer_fields = ToggleCompositeField::create('FoooterInfo', 'Footer',
+        $footer_fields = ToggleCompositeField::create('FooterInfo', 'Footer',
             array(
                 HTMLEditorField::create('FooterContent','Content to appear in footer')->setRows(15)->addExtraClass('stacked')
-            )
-        )->setHeadingLevel(4);
-
-        $contact_fields = ToggleCompositeField::create('ContactInfo', 'Contact Info.',
-            array(
-                TextAreaField::create('ContactAddress', $this->owner->fieldLabel('ContactAddress')),
-                TextField::create('ContactEmail', $this->owner->fieldLabel('ContactEmail')),
-                TextField::create('ContactPhone', $this->owner->fieldLabel('ContactPhone')),
-                TextAreaField::create('MiscContactInfo', $this->owner->fieldLabel('MiscContactInfo')),
-                TextAreaField::create('MapHTML', "HTML to be loaded for mapping info")
             )
         )->setHeadingLevel(4);
 
